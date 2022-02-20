2591b4042510234e162760519826f2228ff130b5
Ashok <ashokbhayal083@gmail.com>
2022-02-20 23:10:32 +0530

    fix in purchase table for existing inventory

--- price_calulator.js
+++ price_calulator.js
@@ -472,9 +472,6 @@ function add_Inventory_Qty()
       {
          console.log("Updated Entry Successfully ", data);
 
-         document.getElementById("add_Qty_textArea").value = "";
-         document.getElementById("add_BarCode_textArea").value = "";
-
          new_Qty = (data.quantity + add_qty);
 
          item_Details_1 = data;
@@ -483,7 +480,11 @@ function add_Inventory_Qty()
          db.run('UPDATE inventory SET quantity=? WHERE barCode=?',[new_Qty, add_barCode]);
          SQL_GB.dbClose(db, dbPath)
 
-         insertInPurchase(item_Details_1);
+         insertInPurchase(item_Details_1, add_qty);
+
+         document.getElementById("add_Qty_textArea").value = "";
+         document.getElementById("add_BarCode_textArea").value = "";
+
 
       }). catch(e =>
       {
@@ -524,11 +525,12 @@ function add_Inventory_Qty()
 }
 
 
-function insertInPurchase(item_Details_1)
+function insertInPurchase(item_Details_1, updatedQty)
 {
    let db = SQL_GB.dbOpen(dbPath);
 
    console.log("Inserting in Purchase table");
+   console.log(item_Details_1);
 
    db.exec('INSERT into purchase(barCode , \
                                  date, \
@@ -544,7 +546,7 @@ function insertInPurchase(item_Details_1)
                                   item_Details_1.description,
                                   item_Details_1.landingPrice,
                                   item_Details_1.sellingPrice,
-                                  item_Details_1.qty,
+                                  updatedQty,
                                   item_Details_1.partyName,
                                   item_Details_1.withBox],
                                   function(err)
